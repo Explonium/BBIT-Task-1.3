59f8c9ff0de8f4a72dc9c4d6f1d86f25dfa08515
Explonium <aleksej.zubov.01@mail.ru>
2021-10-05 17:19:56 +0300

    implemented tenant-details

--- src/app/tenant-list/tenant-details/tenant-details.component.ts
+++ src/app/tenant-list/tenant-details/tenant-details.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ApiService } from 'src/app/shared/api.service';
+import { Form } from 'src/app/shared/models/form.model';
+import { Tenant } from 'src/app/shared/models/tenant.model';
+import { TenantFormService } from 'src/app/shared/tenant-form.service';
 
 @Component({
   selector: 'app-tenant-details',
@@ -7,7 +12,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TenantDetailsComponent implements OnInit {
 
-  constructor() { }
+  formModel: Form;
+  
+  constructor(
+    private api: ApiService,
+    public tenants: TenantFormService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { 
+    let id = this.route.snapshot.paramMap.get('id');
+
+    // Getting model by id
+    this.formModel = new Form(new Tenant(), tenants.getTenantFormDetails());
+    this.formModel.edit = false;
+    this.formModel.model.personalCode = id;
+    this.api.getModelById(this.formModel)
+      .toPromise()
+      .then(res => this.formModel.model = res as Tenant);
+  }
 
   ngOnInit(): void {
   }
